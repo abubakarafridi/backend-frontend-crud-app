d3cccc55b6a7236d989ffec145039d3c123522c8
abubakarafridi <abubakarafridi407@gmail.com>
2024-09-26 15:26:54 +0500

    The server for all the request is completed

---
+++ backend/server.mjs
@@ -0,0 +1,71 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  { id: 1, name: 'abubakar', email: 'abubakar@gmail.com' },
+  { id: 2, name: 'ahmad', email: 'ahmad@gmail.com' },
+];
+
+function errorHandler(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: err.message || 'Something went wrong',
+    error: true,
+  });
+}
+
+app.get('/api/users', (req, res) => {
+  res.status(200).json({ message: 'Get Request - get all user', users });
+});
+
+app.post('/api/users', (req, res) => {
+  const userId = users.length + 1;
+  const newUser = { userId, ...req.body };
+  users.push(newUser);
+  res
+    .status(201)
+    .json({ message: 'Post Request - created a user', data: newUser });
+});
+
+app.put('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.filter((user) => user.id === userId);
+  if (userIndex !== -1) {
+    const updatedUser = { ...users[userIndex], ...req.body };
+    users[userIndex] = updatedUser;
+    res.status(200).json({
+      message: `Update Request - The user with user id ${userId} is updated`,
+      updatedUser,
+    });
+  } else {
+    res
+      .status(404)
+      .json({ message: `The user with user id ${userId} is not found` });
+  }
+});
+
+app.delete('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.filter((user) => user.id === userId);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    res.status(200).json({
+      message: `Delete Request - The user with user id ${userId} is deleted`,
+    });
+  } else {
+    res
+      .status(404)
+      .json({ message: `The user with user id ${userId} is not found` });
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`server is listening on http://localhost:3000`);
+});
